ffd250c0d98dc25b5016dac2d49a2325f13805ff
Šimon Kocúrek <simon.kocurek@gmail.com>
2018-06-20 21:43:31 +0200

    Add sd card preference
    Signed-off-by: Šimon Kocúrek <simon.kocurek@gmail.com>

--- app/src/main/java/simon/trebis/file/FileUtils.kt
+++ app/src/main/java/simon/trebis/file/FileUtils.kt
@@ -2,28 +2,58 @@ package simon.trebis.file
 
 import android.content.Context
 import android.net.Uri
+import android.os.Environment
+import android.support.v7.preference.PreferenceManager
+import simon.trebis.R
 import java.io.File
 
-class FileUtils(private val applicationContext: Context) {
+class FileUtils(private val context: Context) {
 
     fun store(entryId: Long, bitmap: ByteArray) {
-        applicationContext.openFileOutput(entryId.toString(), Context.MODE_PRIVATE).use {
-            it.write(bitmap)
+        val fileStream = if (canUseExternalStorage()) {
+            File(externalStorageDirectory(), entryId.toString()).outputStream()
+        } else {
+            context.openFileOutput(entryId.toString(), Context.MODE_PRIVATE)
         }
 
-        // TODO check SD
+        fileStream.use {
+            it.write(bitmap)
+        }
     }
 
     fun fileUri(entryId: Long): Uri {
-        return Uri.fromFile(File(applicationContext.filesDir, entryId.toString()))
-    }
+        if (canUseExternalStorage()) {
+            val externalFile = File(externalStorageDirectory(), entryId.toString())
+            if (externalFile.exists()) {
+                return Uri.fromFile(externalFile)
+            }
+        }
 
-    fun read(entryId: Long): ByteArray {
-        return applicationContext.openFileInput(entryId.toString()).readBytes()
+        return Uri.fromFile(File(context.filesDir, entryId.toString()))
     }
 
     fun remove(entryId: Long) {
-        applicationContext.deleteFile(entryId.toString())
+        context.deleteFile(entryId.toString())
+
+        if (canUseExternalStorage()) {
+            File(externalStorageDirectory(), entryId.toString()).delete()
+        }
+    }
+
+    private fun canUseExternalStorage(): Boolean {
+        val sdCardMounted = Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED
+        val preferSdCard = PreferenceManager
+                .getDefaultSharedPreferences(context)
+                .getBoolean(context.getString(R.string.prefer_sd), true)
+        return sdCardMounted && preferSdCard
+    }
+
+    private fun externalStorageDirectory(): File {
+        val externalStorage = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val directory = File(externalStorage, context.getString(R.string.app_name))
+        directory.mkdirs()
+
+        return directory
     }
 
 }
